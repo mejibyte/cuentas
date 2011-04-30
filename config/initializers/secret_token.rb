f33d92f19161eecc0c9a18ea2b1aa00ff0d0c900
Andrés Mejía <andmej@gmail.com>
2011-04-30 11:26:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cuentas::Application.config.secret_token = 'fca8661113dfbc23f4aea140e6379bb72e8d71c77b6584161850ecc3b29bbc6982412839d134218f60ca11a687dc1751f2cec281ddfe678810c9b62d9c2a7bd0'
