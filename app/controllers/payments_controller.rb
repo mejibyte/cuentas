a726137edd16ced1c1d414d135181cd2d65363c6
Andrés Mejía <andmej@gmail.com>
2011-05-04 20:14:41 -0500

    Translating some stuff on the controller.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class PaymentsController < ApplicationController
   def index
     @payments = Payment.all
@@ -14,7 +15,7 @@ class PaymentsController < ApplicationController
   def create
     @payment = Payment.new(params[:payment])
     if @payment.save
-      flash[:notice] = "Successfully created payment."
+      flash[:notice] = "Se agregó el pago."
       redirect_to @payment
     else
       render :action => 'new'
@@ -28,7 +29,7 @@ class PaymentsController < ApplicationController
   def update
     @payment = Payment.find(params[:id])
     if @payment.update_attributes(params[:payment])
-      flash[:notice] = "Successfully updated payment."
+      flash[:notice] = "Se actualizó el pago."
       redirect_to payment_url
     else
       render :action => 'edit'
@@ -38,7 +39,7 @@ class PaymentsController < ApplicationController
   def destroy
     @payment = Payment.find(params[:id])
     @payment.destroy
-    flash[:notice] = "Successfully destroyed payment."
+    flash[:notice] = "Se borró el pago."
     redirect_to payments_url
   end
 end
