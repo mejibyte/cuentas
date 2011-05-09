58461f14803a1b75b93210d04a13dda3d60d280e
Andrés Mejía <andmej@gmail.com>
2011-05-08 23:48:49 -0500

    Changing the from field in emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class UserMailer < ActionMailer::Base
-  default :from => "andmej@gmail.com"
+  default :from => "Nuestro Apartamento <andmej@gmail.com>"
     
   def new_payment_made(payment)
     @payment = payment
